be3438d5275960edfd2a0bd297471a36fdbbc2b5
PietroColaguori <pietro.colaguori3889@gmail.com>
2023-08-23 23:25:51 +0200

    Uploaded files for pseudo_malloc
    Use buddy allocator if request < page_size / 4, else use mmap. Also tried to implement free and calloc.

---
+++ pseudo_malloc.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <sys/mman.h>
+#include <math.h>
+
+#include "bit_map.h"
+#include "buddy_allocator.h"
+#include "pseudo_malloc.h"
+
+void* pseudo_malloc(int size) {
+    assert(size > 0);
+    void* allocated_mem;
+    size += sizeof(int);
+
+    // small allocation, we will use the buddy allocator
+    if(size < PAGE_SIZE / 4) {
+        allocated_mem = BuddyAllocator_malloc(&balloc, size);
+    }
+
+    // big allocation, we will use mmap
+    else {
+        printf("The needed size %d is greater than %d\n", size, PAGE_SIZE / 4);
+        int* allocated_mem = (int*) mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        assert(allocated_mem != MAP_FAILED);
+        allocated_mem[0] = size;
+        printf("Address %p of size %d allocated usinng mmap\n", allocated_mem, size);
+        return (void*)(allocated_mem + 1);
+    }
+
+    return allocated_mem;
+}
+
+void pseudo_free(void* address, int size) {
+    assert(address != NULL);
+    int adjusted_size = size + sizeof(int);
+
+    // The address was allocated using a buddy allocator
+    if (size < PAGE_SIZE / 4) {
+        printf("--- %d < %d --- [buddy]\n", adjusted_size, PAGE_SIZE / 4);
+        BuddyAllocator_freeAllocatedMemory(&balloc, address);
+    }
+    // The address was allocated using mmap
+    else {
+        printf("\n--- %d > %d --- [munmap]\n", size, PAGE_SIZE / 4);
+        int* adjusted_address = (int*)address - 1;
+        assert(munmap((void*)adjusted_address, adjusted_size) != -1);
+        printf("Address %p of size %d deallocated using munmap\n", adjusted_address, adjusted_size);
+    }
+}
+
+void* pseudo_calloc(int num_elements, int size) {
+    void* allocated_mem = pseudo_malloc(num_elements * size);
+    memset(allocated_mem, 0, num_elements * size);
+    printf("Allocated and zeroed %d contigous blocks of size %d at address %p\n", num_elements, size, allocated_mem);
+    return allocated_mem;
+}
---
+++ pseudo_malloc.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <sys/mman.h>
+#include <math.h>
+
+#define PAGE_SIZE 4096
+#define MANAGED_MEMORY_SIZE 1 << 20
+#define NUM_LEVELS 21
+#define MIN_BUCKET_SIZE (MANAGED_MEMORY_SIZE >> (NUM_LEVELS - 1))
+#define NUM_BUDDIES ((1 << NUM_LEVELS) - 1)
+#define NUM_BITS_BITMAP ((NUM_BUDDIES + 7) << 3)
+
+void* pseudo_malloc(int size);
+void pseudo_free(void* address, int size);
+void* pseudo_calloc(int num_elements, int size);
